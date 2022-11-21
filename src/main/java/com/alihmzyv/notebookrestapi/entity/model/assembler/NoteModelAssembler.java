d205ace7b8a293bad75f1a122931901b804964b9
alihmzyv <alihmzyv@gmail.com>
2022-11-22 02:31:17 +0400

    note contains link to user now

--- src/main/java/com/alihmzyv/notebookrestapi/entity/model/assembler/NoteModelAssembler.java
+++ src/main/java/com/alihmzyv/notebookrestapi/entity/model/assembler/NoteModelAssembler.java
@@ -2,20 +2,28 @@ package com.alihmzyv.notebookrestapi.entity.model.assembler;
 
 import com.alihmzyv.notebookrestapi.controller.NoteController;
 import com.alihmzyv.notebookrestapi.entity.Note;
+import com.alihmzyv.notebookrestapi.entity.User;
 import com.alihmzyv.notebookrestapi.entity.model.NoteModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NoteModelAssembler extends RepresentationModelAssemblerSupport<Note, NoteModel> {
 
-    public NoteModelAssembler() {
+    private UserModelAssembler userModelAssembler;
+
+    @Autowired
+    public NoteModelAssembler(UserModelAssembler userModelAssembler) {
         super(NoteController.class, NoteModel.class);
+        this.userModelAssembler = userModelAssembler;
     }
 
     @Override
     public NoteModel toModel(Note entity) {
-        return createModelWithId(entity.getId(), entity);
+        return createModelWithId(entity.getId(), entity)
+                .add(userModelAssembler.toModel(entity.getUser()).getRequiredLink("self")
+                        .withRel("user"));
     }
 
     @Override
