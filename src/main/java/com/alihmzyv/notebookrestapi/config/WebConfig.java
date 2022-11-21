2283784d6db296b6bd7b7c544a30d6c223d5cf0c
alihmzyv <alihmzyv@gmail.com>
2022-11-22 01:14:36 +0400

    redirect modified

--- src/main/java/com/alihmzyv/notebookrestapi/config/WebConfig.java
+++ src/main/java/com/alihmzyv/notebookrestapi/config/WebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addRedirectViewController("/", "/swagger-ui.html");
+        registry.addRedirectViewController("", "/swagger-ui.html");
+        registry.addRedirectViewController("/", "swagger-ui.html");
     }
 }
