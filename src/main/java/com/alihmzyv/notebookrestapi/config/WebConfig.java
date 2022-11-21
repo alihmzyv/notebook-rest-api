943e39a52f454b1121fd3f9b28957c52456742bd
alihmzyv <alihmzyv@gmail.com>
2022-11-22 01:16:04 +0400

    problem with redirect

--- src/main/java/com/alihmzyv/notebookrestapi/config/WebConfig.java
+++ src/main/java/com/alihmzyv/notebookrestapi/config/WebConfig.java
@@ -9,6 +9,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addRedirectViewController("", "/swagger-ui.html");
-        registry.addRedirectViewController("/", "swagger-ui.html");
+        registry.addRedirectViewController("/", "/swagger-ui.html");
     }
 }
