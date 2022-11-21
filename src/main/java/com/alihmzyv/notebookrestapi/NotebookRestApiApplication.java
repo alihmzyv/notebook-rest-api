c3bf52ca4f5af52f8f2e39b764f177ac2f662198
alihmzyv <alihmzyv@gmail.com>
2022-11-22 00:45:50 +0400

    first commit

--- src/main/java/com/alihmzyv/notebookrestapi/NotebookRestApiApplication.java
+++ src/main/java/com/alihmzyv/notebookrestapi/NotebookRestApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NotebookRestApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(NotebookRestApiApplication.class, args);
 	}
