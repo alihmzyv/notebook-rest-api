5d84d28fcfb9287ddbd481d19c97b3cf9f129f31
alihmzyv <alihmzyv@gmail.com>
2022-11-22 13:42:13 +0400

    findAll added

--- src/main/java/com/alihmzyv/notebookrestapi/service/NoteService.java
+++ src/main/java/com/alihmzyv/notebookrestapi/service/NoteService.java
@@ -2,7 +2,10 @@ package com.alihmzyv.notebookrestapi.service;
 
 import com.alihmzyv.notebookrestapi.entity.Note;
 
+import java.util.List;
+
 public interface NoteService {
+    List<Note> findAll(int page, int size, List<String> sort);
     Note findNoteById(Long noteId);
     Note updateNote(Long noteId, Note note);
     void deleteNoteById(Long noteId);
