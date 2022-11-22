a03f0336f55798a0ee0dced2692411fb7c2f81f3
alihmzyv <alihmzyv@gmail.com>
2022-11-22 13:44:24 +0400

    sortinghelper to help to parse sort parameters

---
+++ src/main/java/com/alihmzyv/notebookrestapi/service/SortingHelper.java
@@ -0,0 +1,26 @@
+package com.alihmzyv.notebookrestapi.service;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SortingHelper {
+    public List<Sort.Order> createSortOrder(List<String> sort) {
+        if (sort.size() == 2
+                && (sort.get(1).equalsIgnoreCase("asc") || sort.get(1).equalsIgnoreCase("desc"))) {
+            return List.of(new Sort.Order(Sort.Direction.fromString(sort.get(1)), sort.get(0)));
+        }
+        System.out.println(sort);
+        return sort.stream()
+                .map(sortAndOrder -> sortAndOrder.split(","))
+                .map(sortAndOrder -> {
+                    if (sortAndOrder.length == 1) {
+                        return new Sort.Order(Sort.Direction.ASC, sortAndOrder[0]);
+                    }
+                    return new Sort.Order(Sort.Direction.fromString(sortAndOrder[1]), sortAndOrder[0]);
+                })
+                .toList();
+    }
+}
