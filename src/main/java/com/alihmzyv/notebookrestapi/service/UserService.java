ccb859ad863ec75876514b9f843691299090d7f3
alihmzyv <alihmzyv@gmail.com>
2022-11-22 13:43:53 +0400

    new methods

--- src/main/java/com/alihmzyv/notebookrestapi/service/UserService.java
+++ src/main/java/com/alihmzyv/notebookrestapi/service/UserService.java
@@ -6,11 +6,12 @@ import com.alihmzyv.notebookrestapi.entity.User;
 import java.util.List;
 
 public interface UserService {
+    List<User> findAll(int page, int size, List<String> sort);
     User findUserById(Long userId);
     User findUserByEmailAddressOrUsernameAndPassword(String emailAddressOrUsername, String password);
     void saveUser(User user);
     void deleteUserById(Long userId);
-    List<Note> findNotesByUserId(Long userId);
+    List<Note> findNotesByUserId(Long userId, int page, int size, List<String> sort);
     void createNote(Long userId, Note note);
     void requiresUserExistsById(Long userId);
 }
