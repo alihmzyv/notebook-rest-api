5e2e40c9847adf6454b135028e227d9921a21983
alihmzyv <alihmzyv@gmail.com>
2022-11-22 13:41:36 +0400

    findNotesByUserId

--- src/main/java/com/alihmzyv/notebookrestapi/repo/NoteRepository.java
+++ src/main/java/com/alihmzyv/notebookrestapi/repo/NoteRepository.java
@@ -1,9 +1,13 @@
 package com.alihmzyv.notebookrestapi.repo;
 
 import com.alihmzyv.notebookrestapi.entity.Note;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
+    List<Note> findAllByUserId(Long userId, Pageable pageable);
 }
