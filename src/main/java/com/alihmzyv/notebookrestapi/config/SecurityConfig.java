13228bcbf902461fec35691b80493b9421a5998f
alihmzyv <alihmzyv@gmail.com>
2022-11-24 03:42:15 +0400

    disable csrf, get bcrypt encoder as bean

--- src/main/java/com/alihmzyv/notebookrestapi/config/SecurityConfig.java
+++ src/main/java/com/alihmzyv/notebookrestapi/config/SecurityConfig.java
@@ -1,12 +1,14 @@
 package com.alihmzyv.notebookrestapi.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
@@ -20,18 +22,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         this.dataSource = dataSource;
     }
 
+    @Bean
+    public PasswordEncoder bcryptEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.jdbcAuthentication()
                 .dataSource(dataSource)
-                .passwordEncoder(new BCryptPasswordEncoder())
+                .passwordEncoder(bcryptEncoder())
                 .usersByUsernameQuery("select username, password, enabled from api_users where username=?")
                 .authoritiesByUsernameQuery("select username, role from api_users where username=?");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.cors().and().csrf().disable()
+                .authorizeRequests()
                 .antMatchers("/")
                 .permitAll()
                 .anyRequest()
