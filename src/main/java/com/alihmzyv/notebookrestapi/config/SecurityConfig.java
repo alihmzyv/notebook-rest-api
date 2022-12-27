f6c21f7c7ba6948d6306e2990fc863a314104657
alihmzyv <alihmzyv@gmail.com>
2022-12-27 21:46:22 +0400

    use api datasource for security

--- src/main/java/com/alihmzyv/notebookrestapi/config/SecurityConfig.java
+++ src/main/java/com/alihmzyv/notebookrestapi/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.alihmzyv.notebookrestapi.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -18,7 +19,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final DataSource dataSource;
 
     @Autowired
-    public SecurityConfig(DataSource dataSource) {
+    public SecurityConfig(@Qualifier("api-datasource") DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
